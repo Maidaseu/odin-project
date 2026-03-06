78ef404235ad098e1c9ec587fd9e6a59ce87baa9
Maidaseu <248063980+Maidaseu@users.noreply.github.com>
2026-03-06 17:56:34 +0000

    Step 2: Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// Test that JavaScript is linked
-console.log("Hello World");
+// Write out the logic for the game
+function getComputerChoice() {
+  const random = Math.random();
+  if (random < 0.33) {
+    return "rock";
+  } else if (random < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+
+  // Test that the function
+  console.log(getComputerChoice());
+}
