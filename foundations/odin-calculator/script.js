d437619444fa3bd3233341f06a191338ac3a2359
Maidaseu <248063980+Maidaseu@users.noreply.github.com>
2026-04-23 09:34:44 +0100

    Add basic arithmetic functions (add, subtract, multiply, divide)

---
+++ foundations/odin-calculator/script.js
@@ -0,0 +1,37 @@
+// Executes a calculation based on the given operator and operands
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) return "Nice try. Can't divide by 0 😏";
+  return a / b;
+}
+
+// Select the correct operation based on the operator
+function operate(operator, a, b) {
+  // Convert inputs (UI gives strings)
+  a = Number(a);
+  b = Number(b);
+
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return null;
+  }
+}
